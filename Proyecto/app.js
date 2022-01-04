40b2a5a9c8d80c60ed0139653866d42a2a47eab3
Elemental99 <kevinponcesite@gmail.com>
2022-01-04 16:16:08 -0500

    hola

---
+++ Proyecto/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const Handlebars = require("handlebars");
+
+const app = express();
+
+//Invocamos a dotenv: Variables de entorno
+dotenv.config({ path: "./env/.env" });
+
+//Invocamos modulo de la conexion a DB
+// const db = require("./database/db");
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+    ".hbs",
+    exphbs({
+        defaultLayout: "main",
+        layoutsDir: path.join(app.get("views"), "layouts"),
+        partialsDir: path.join(app.get("views"), "partials"),
+        extname: ".hbs",
+    })
+);
+app.set("view engine", ".hbs");
+
+Handlebars.registerHelper("ife", function (lvalue, rvalue, options) {
+    if (lvalue === rvalue) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
+//Public - archivos estaticos
+app.use(express.static("public"));
+
+//procesar datos enviados desde form
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Para poder trabajar con las cookies
+app.use(cookieParser());
+
+//Define routes
+app.use("/", require("./routes/auth"));
+
+//Para eliminar el cache y que no se pueda volver con el boton de back
+app.use(function (req, res, next) {
+    if (!req.nombre_usuario) {
+        res.header(
+            "Cache-Control",
+            "private, no-cache, no-store, must-revalidate"
+        );
+    }
+    next();
+});
+
+app.listen(4000, () => {
+    console.log("Server started on port http://localhost:4000");
+});
---
+++ Proyecto/controllers/auth.js
@@ -0,0 +1,334 @@
+const db = require("../database/db");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+exports.consultregister = (req, res) => {
+    db.query("SELECT * FROM rol", (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            req.datos = results;
+            res.render("register", {
+                datos: req.datos,
+            });
+        }
+    });
+};
+
+//Registro
+exports.register = async (req, res) => {
+    try {
+        const {
+            nombres,
+            apellidos,
+            nombre_usuario,
+            contrasena,
+            contrasena2,
+            rol,
+            correo,
+            direccion,
+        } = req.body;
+
+        if (
+            !nombres ||
+            !apellidos ||
+            !nombre_usuario ||
+            !contrasena ||
+            !contrasena2 ||
+            !rol ||
+            !correo ||
+            !direccion
+        ) {
+            res.render("register", {
+                message: "Ingrese todos los campos",
+            });
+        } else {
+            db.query(
+                "SELECT * FROM usuario WHERE nombre_usuario = ?",
+                [nombre_usuario],
+                async (error, results) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        if (results.length > 0) {
+                            return res.render("register", {
+                                message: "Usuario en uso",
+                            });
+                        } else if (contrasena != contrasena2) {
+                            return res.render("register", {
+                                message: "Contraseñas no coinciden",
+                            });
+                        }
+                        db.query(
+                            "INSERT INTO usuario SET ?",
+                            {
+                                id_rol: rol,
+                                nombre: nombres,
+                                apellido: apellidos,
+                                nombre_usuario: nombre_usuario,
+                                correo: correo,
+                                contrasena: contrasena,
+                                direccion: direccion,
+                            },
+                            (error, results) => {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    return res.render("register", {
+                                        message: "Usuario registrado",
+                                    });
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//Inicio de sesion
+exports.login = async (req, res) => {
+    try {
+        const { nombre_usuario, contrasena } = req.body;
+        if (!nombre_usuario || !contrasena) {
+            return res.render("login", {
+                message: "Ingrese nombre de Usuario y/o contraseña",
+            });
+        } else {
+            await db.query(
+                "SELECT * FROM usuario WHERE nombre_usuario = ?",
+                [nombre_usuario],
+                async (error, results) => {
+                    if (results.length == 0) {
+                        return res.render("login", {
+                            message:
+                                "Nombre de Usuario y/o contraseña incorrecta",
+                        });
+                    } else {
+                        const id_usuario = results[0].id_usuario;
+                        const token = jwt.sign(
+                            { id_usuario: id_usuario },
+                            process.env.JWT_SECRET,
+                            {
+                                expiresIn: process.env.JWT_TIME_EXPIRE,
+                            }
+                        );
+
+                        const cookiesOption = {
+                            expires: new Date(
+                                Date.now() +
+                                process.env.JWT_COOKIE_EXPIRE *
+                                24 *
+                                60 *
+                                60 *
+                                1000
+                            ),
+                            httpOnly: true,
+                        };
+
+                        res.cookie("jwt", token, cookiesOption);
+                        return res.redirect("/");
+                    }
+                }
+            );
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//Autenticacion
+exports.isAuthenticated = async (req, res, next) => {
+    if (req.cookies.jwt) {
+        try {
+            const decodify = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
+            await db.query(
+                "SELECT * FROM usuario INNER JOIN rol ON usuario.id_rol = rol.id_rol " +
+                "WHERE id_usuario = ?",
+                [decodify.id_usuario],
+                (error, results) => {
+                    if (!results) {
+                        return next();
+                    }
+                    if (results[0].id_rol == 1 || results[0].id_rol == 2) {
+                        req.estud = 0;
+                    } else {
+                        req.estud = 1;
+                    }
+                    req.nombre_usuario = results[0];
+                    return next();
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        return res.render("index");
+    }
+};
+
+//Cierre de sesion
+exports.logout = (req, res) => {
+    res.clearCookie("jwt");
+    return res.render("index");
+};
+
+//Actualizar datos
+exports.editar = (req, res) => {
+    try {
+        const {
+            nombre,
+            apellido,
+            nombre_usuario,
+            contrasena,
+            correo,
+            direccion,
+        } = req.body;
+
+        const datos = {
+            nombre,
+            apellido,
+            contrasena,
+            correo,
+            direccion,
+        };
+
+        if (!nombre || !apellido || !contrasena || !correo || !direccion) {
+            res.render("actualizar", {
+                message: "Ingrese todos los campos",
+            });
+        } else {
+            db.query(
+                "UPDATE usuario SET ? WHERE nombre_usuario = ?",
+                [datos, nombre_usuario],
+                (error, results) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        
+                        
+                        return res.render("profile", {
+                            nombre_usuario: req.nombre_usuario,
+                            message: "Datos Actualizados",
+                        });
+                    }
+                    req.dato = results[0]
+                    console.log(req.dato)
+                }
+            );
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//Consulta de horario
+exports.consultvista = async (req, res) => {
+    db.query(
+        "SELECT * FROM nivel INNER JOIN paralelo p ON nivel.id_nivel = p.id_nivel",
+        (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                req.datos = results;
+                res.render("vistausuario", {
+                    nombre_usuario: req.nombre_usuario,
+                    datos: req.datos,
+                });
+            }
+        }
+    );
+};
+
+//Contact
+exports.contact = (req, res) => {
+    const { nombre, telefono, correo, mensaje } = req.body;
+    if (!nombre || !telefono || !correo || !mensaje) {
+        res.render("contact", {
+            nombre_usuario: req.nombre_usuario,
+            message: "Ingrese todos los campos",
+        });
+    } else {
+        res.render("contact", {
+            nombre_usuario: req.nombre_usuario,
+            message: "Datos enviados correctamente",
+        });
+    }
+};
+
+//Consulta
+exports.consultNuevoHorario = (req, res) => {
+    db.query(
+        "SELECT * FROM usuario WHERE id_rol = 2",
+        (error, results) => {
+            if (error) {
+                console.log(error);
+            }
+            req.docente = results;
+            db.query('SELECT * FROM nivel', (error, resp) => {
+                if (error) {
+                    console.log(error);
+                }
+                req.nivel = resp;
+                res.render("nuevohorario", {
+                    nombre_usuario: req.nombre_usuario,
+                    docente: req.docente,
+                    nivel: req.nivel
+                })
+            });
+        }
+    );
+}
+
+//Consulta Generar Horario
+exports.consultGenerar = (req, res) => {
+    const { nivel } = req.body
+    db.query(
+        "SELECT * FROM paralelo WHERE id_nivel = ?", [nivel],
+        (error, results) => {
+            if (error) {
+                console.log(error);
+            }
+            req.paralelo = results;
+            db.query('SELECT * FROM materia WHERE id_nivel = ?', [nivel], (error, resp) => {
+                if (error) {
+                    console.log(error);
+                }
+                req.materia = resp;
+                res.render('GenerarHorario', {
+                    nombre_usuario: req.nombre_usuario,
+                    paralelo: req.paralelo,
+                    materia: req.materia
+                })
+            });
+        }
+    );
+}
+
+//Generar Horario
+exports.GenerarHorario = (req, res) => {
+    const { hora_inicio, hora_fin, materia, dia_s } = req.body
+    if (!hora_fin || !hora_inicio || !materia || !dia_s) {
+        res.render("GenerarHorario", {
+            message: "Ingrese todos los campos",
+        });
+    } else {
+        db.query('INSERT INTO horario set ?',
+            { id_materia: materia, dia_s: dia_s, hora_ini: hora_inicio, hora_fin: hora_fin },
+            (error, results) => {
+                if (error) {
+                    console.log(error)
+                }
+                res.render('nuevohorario', {
+                    nombre_usuario: req.nombre_usuario,
+                    message: 'Horario generado'
+                })
+            })
+    }
+}
